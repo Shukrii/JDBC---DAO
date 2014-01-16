a70958ced9f54bc86be1dcaa9feb311097ecc5da
Shukrii <nez14381@spengergasse.at>
2014-01-16 12:15:33 +0100

    Update Main

--- JDBC-DAO/src/sonstiges/Main.java
+++ JDBC-DAO/src/sonstiges/Main.java
@@ -14,20 +14,24 @@ import domain.*;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		KlassenDAOImpl    k = new KlassenDAOImpl();
 		LehrerDAOImpl     l = new LehrerDAOImpl();
 		UnterrichtDAOImpl u = new UnterrichtDAOImpl();
 		
 		
-		//Testdatensätze
+		//Lehrer
 		Lehrer l1 = new Lehrer(1, "John", "Johnas");
 		Lehrer l2 = new Lehrer(2, "Max", "Min");
 		Lehrer l3 = new Lehrer(2, "Jason", "Statham");
 
-		
+		//Klassen
 		Klassen k1 = new Klassen ("1BFIT");
 		Klassen k2 = new Klassen ("3ASHT");
+		Klassen k3 = new Klassen ("3AHIF");
+		Klassen k4 = new Klassen ("3BHIF");
 		
+		//Unterricht
 		Unterricht u1 = new Unterricht(l1.getId(), k1.getId(),"UO1");
 		Unterricht u2 = new Unterricht(l1.getId(), k1.getId(),"UO2");
 		Unterricht u3 = new Unterricht(l2.getId(), k1.getId(),"UO4");
@@ -40,65 +44,44 @@ public class Main {
 			
 			ResultSet rs = stmt.executeQuery("SELECT * FROM klassen;");
 		
-			System.out.println("---Nach Insert von den 2 Klassen---");
+			System.out.println("---Insert von den 4 Klassen---");
 			rs = stmt.executeQuery("SELECT * FROM klassen;");
 			k.insert(con, k1);
 			k.insert(con, k2);
-			showRS(rs,1);
+			k.insert(con, k3);
+			k.insert(con, k4);
+			showRS(rs,4);
 			
 			l.insert(con, l1);
 			l.insert(con, l2);
 			l.insert(con, l3);
-			System.out.println("---Nach Insert von den 3 Lehrern---");
+			System.out.println("---Insert von den 3 Lehrern---");
 			rs = stmt.executeQuery("SELECT * FROM lehrer;");
 			showRS(rs,3);
 			
 			u.insert(con, u1);
 			u.insert(con, u2);
 			u.insert(con, u3);
-			System.out.println("---Nach Insert von den 3 Unterricht---");
+			System.out.println("---Insert von den 3 Unterricht---");
 			rs = stmt.executeQuery("SELECT * FROM unterricht;");
 			showRS(rs,3);
+		
 			
-			System.out.println("---Nach Update von Klasse 1---");
-			rs = stmt.executeQuery("Update klassen set id = '1BFIT' where id = '1BFIT';");
+			System.out.println("---Update von Lehrer ID 1---");
+			rs = stmt.executeQuery("Update lehrer set Vorname = 'Johannes' where id = '1';");
+			showRS(rs,3);
+			
+			System.out.println("---Update von Klasse 3BHIF---");
+			rs = stmt.executeQuery("Update klassen set id = '4BHIF' where id = '3BHIF';");
+			showRS(rs,3);
+			
+			System.out.println("---Delete von Klasse 3ASHT---");
+			rs = stmt.executeQuery("delete from klassen where id = '3ASHT'");
+			showRS(rs,3);
+			
+			System.out.println("---Update von Lehrer Id2---");
+			rs = stmt.executeQuery("Update lehrer set vorname = 'Mustermann' where id = '2';");
 			showRS(rs,1);
-			
-			System.out.println("---Nach Update von Lehrer 2---");
-			rs = stmt.executeQuery("Update lehrer set Vorname = 'John' where id = 'Johnas';");
-			showRS(rs,3);
-			
-			System.out.println("---Nach Update von Unterricht 2---");
-			rs = stmt.executeQuery("Update Unterricht set tagstunde = 'DI1' where tagstunde = 'UO2';");
-			showRS(rs,3);
-			
-			System.out.println("---Nach Delete von Unterricht 2---");
-			rs = stmt.executeQuery("delete from Unterricht where tagstunde = 'DI1'");
-			showRS(rs,3);
-			
-			System.out.println("---Truncate von unterricht---");
-			rs = stmt.executeQuery("truncate unterricht");
-			try{
-				rs.first();
-			}catch(SQLException ex){
-				System.out.println("Im Catch-Block nach rs.first() -> tabelle leer");
-			}
-			
-			System.out.println("---löschen aller Klassen---");
-			rs = stmt.executeQuery("delete from klassen where 1 = 1");
-			try{
-				rs.first();
-			}catch(SQLException ex){
-				System.out.println("Im Catch-Block nach rs.first() -> tabelle leer");
-			}
-			
-			System.out.println("---löschen aller lehrer---");
-			rs = stmt.executeQuery("delete from lehrer where 1 = 1");
-			try{
-				rs.first();
-			}catch(SQLException ex){
-				System.out.println("Im Catch-Block nach rs.first() -> tabelle leer");
-			}
 			
 			rs.close();
 			stmt.close();
